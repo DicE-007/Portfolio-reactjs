75aef794f60a4d6cde8846791073101f8866e26c
Aryaveer <aryaveerchaudhary007@gmail.com>
2022-08-18 19:28:21 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Home from "./components/home.component";
+import NavBar from "./components/navbar.component";
+import SocialLinks from "./components/social-links.component";
+import About from "./components/about.component.jsx"
+import PortFolio from "./components/portfolio.component";
+import Experience from "./components/experience.component";
+import Contact from "./components/contact.component";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <Home/>
+      <About/>
+      <PortFolio/>
+      <Experience/>
+      <Contact/>
+      <SocialLinks/>
     </div>
+   
   );
 }
 
---
+++ src/components/about.component.jsx
@@ -0,0 +1,35 @@
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          I am an experienced full stack developer from Ghaziabad India working
+          with reactjs. Additionally trained with python , C++ , Django. I have
+          made various projects in react and django in my first and second year
+          of graduation time. i have a goal in which i want to keep learning ,
+          achieving and always take failure as a one step towards success.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          My hunger for knowledge gave me enough determination to turn my ideas
+          in to live projects.My most recent project "Crown Clothing" is the
+          proof of my determination in which i used reactjs,firebase,hooks and
+          redux.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
